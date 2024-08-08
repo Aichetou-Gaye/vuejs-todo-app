d9c526835a53c34e34b317780f6a9fcb89ef489b
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-08 10:53:18 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>Vue 3 Todo App</h1>
-    <form @submit.prevent ="addNewTodo">
+    <form @submit.prevent="addNewTodo">
 
       <label>New Todo</label>
       <input v-model="newTodo" name="newTodo">
       <button>Add New Todo</button>
     </form>
-    <h2>{{ newTodo }}</h2>
+    <button @click="removeAll()">Remove All Todos</button>
+    <button @click="markAllDone()">Mark All Done</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="todo.id">
+        <h3 :class="{done: todo.done}" @click="toggleDone(todo)" class="todo">{{ todo.content }}</h3>
+        <button @click="removeTodo(index)" class="remove">Remove Todo</button>
+      </li>
+    </ul>
+
   </div>
 </template>
 
@@ -15,26 +23,48 @@
 import { ref } from 'vue';
 
 const newTodo = ref('')
+const todos = ref([])
+
 function addNewTodo() {
-  console.log(newTodo.value)
+  todos.value.push({
+    id: Date.now(),
+    done: false,
+    content: newTodo.value,
+  })
+  newTodo.value = ''
 }
 
-// return {
-//   addNewTodo,
+function toggleDone(todo) {
+  todo.done = !todo.done
+}
 
+function removeTodo(index) {
+  todos.value.splice(index, 1)
+}
+function markAllDone() {
+  todos.value.forEach((todo) => todo.done = true)
+}
+
+function removeAll() {
+  todos.value = []
+}
+// return {
+//  newTodo,
+//   addNewTodo,
 // }
 
 </script>
 
 <style scoped>
-body{
-  font-family : sans-serif;
+body {
+  font-family: sans-serif;
   padding-top: 1em;
   padding-bottom: 1em;
   font-size: 2em;
   width: 80%;
   margin: 0 auto;
 }
+
 h1 {
   color: rgb(5, 100, 100);
   font-weight: bolder;
@@ -42,33 +72,51 @@ h1 {
   font-size: 3em;
   margin-bottom: 30px;
 }
+
 form {
   display: flex;
   flex-direction: column;
   gap: 10px;
 }
+
 label {
   font-size: 20px;
   font-weight: 600;
 }
+
 input {
   box-shadow: 1px 2px 2px 1px rgb(5, 100, 100);
   height: 8vh;
   border: none;
   border-radius: 10px
 }
+
 button {
   background-color: rgb(5, 100, 100);
   color: white;
-  height: 5vh;
-  width: 40%;
+  height: 6vh;
   border: none;
   border-radius: 5px;
-  font-size: 1em;
   font-weight: 800;
-  font-family : sans-serif;
   align-self: center;
-  margin-top: 15px;
+  margin: 20px 0;
   cursor: pointer;
+}
+
+input, textarea, button, p, div, section,article, select {
+  display: block;
+  width: 100%;
+  font-family: sans-serif;
+  /* font-size: 1em;
+  margin: 0.5em; */
+}
+.todo{
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
+}
+button:active{
+  transform: scale(1.1);
 }
 </style>
